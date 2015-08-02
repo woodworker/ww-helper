cb8d22e95d23ef1b1415dfddaa00c8112ec8ee04
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-08-02 17:29:25 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/string.php
+++ src/string.php
@@ -7,7 +7,7 @@ namespace woodworker\string;
  * @return bool
  */
 function startsWith($haystack, $needle) {
-    if(!is_string($haystack) || !is_string($needle)) {
+    if (!is_string($haystack) || !is_string($needle)) {
         throw new \InvalidArgumentException('');
     }
     return $needle === '' || strpos($haystack, $needle) === 0;
@@ -19,7 +19,7 @@ function startsWith($haystack, $needle) {
  * @return bool
  */
 function endsWith($haystack, $needle) {
-    if(!is_string($haystack) || !is_string($needle)) {
+    if (!is_string($haystack) || !is_string($needle)) {
         throw new \InvalidArgumentException();
     }
     return $needle === "" || substr($haystack, -strlen($needle)) === $needle;
